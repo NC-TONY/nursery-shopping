e4ab6ffa5495f730c68bdce2bcb26ae6ceb199b9
NC-TONY <154887453+NC-TONY@users.noreply.github.com>
2025-06-05 17:24:17 +0000

    app fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import LandingPage from './pages/LandingPage';
 import ProductsPage from './pages/ProductsPage';
@@ -11,14 +10,12 @@ import './App.css';
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
     </CartProvider>
   );
 }
